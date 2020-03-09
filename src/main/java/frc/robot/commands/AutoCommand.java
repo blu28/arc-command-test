f975b8aa3ff0b6f26357961503b687f8f67cf1f9
Brian Utterback <brian@utterback.org>
2020-03-09 10:48:52 -0400

    Add final, cleanup

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import static frc.robot.Constants.AutoConstants.kAutoDriveTime;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.AutoConstants;
@@ -7,12 +9,18 @@ import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoCommand extends SequentialCommandGroup {
     private final DriveSubsystem m_subsystem;
+    private boolean done_yet = false;
 
-    public AutoCommand(DriveSubsystem subsystem) {
+    public AutoCommand(final DriveSubsystem subsystem) {
         m_subsystem = subsystem;
-        addCommands(
-            new WaitCommand(AutoConstants.kAutoWaitSeconds),
-            new DriveCommand(m_subsystem, AutoConstants.kAutoSpeed, 0).withTimeout(AutoConstants.kAutoWaitSeconds)
+        addCommands(new WaitCommand(AutoConstants.kAutoWaitSeconds),
+            new DriveCommand(m_subsystem, AutoConstants.kAutoSpeed, 0).withTimeout(kAutoDriveTime),
+            new DriveCommand(m_subsystem, 0.0, 0.0)
         );
+        done_yet = true;
     };
+    @Override
+    public boolean isFinished () {
+        return done_yet;
+    }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.wpilibj.GenericHID.Hand.kLeft;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -10,18 +12,18 @@ public class DriveSubsystem extends SubsystemBase {
     private final SpeedController m_rightmotor;
     private final DifferentialDrive m_diffdrive;
 
-    public DriveSubsystem(SpeedController leftmotor, SpeedController rightmotor) {
+    public DriveSubsystem(final SpeedController leftmotor, final SpeedController rightmotor) {
         m_leftmotor = leftmotor;
         m_rightmotor = rightmotor;
         m_diffdrive = new DifferentialDrive(m_leftmotor, m_rightmotor);
     }
-    public void arcadeDrive(double fwd, double rot) {
+
+    public void arcadeDrive(final double fwd, final double rot) {
         m_diffdrive.arcadeDrive(fwd, rot);
     }
-    public void arcadeDrive(GenericHID m_stick) {
-        m_diffdrive.arcadeDrive(
-            m_stick.getY(GenericHID.Hand.kLeft),
-            m_stick.getX(GenericHID.Hand.kLeft)
+
+    public void arcadeDrive(final GenericHID m_stick) {
+        m_diffdrive.arcadeDrive(m_stick.getY(GenericHID.Hand.kLeft), m_stick.getX(kLeft)
         );
     }
 }
